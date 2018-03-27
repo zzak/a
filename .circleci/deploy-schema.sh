856bae0a209a8863dd05ba79aac1d888011672c1
Zachary Scott <mail@zzak.io>
2018-03-27 15:02:12 +0900

    Add git config

--- .circleci/deploy-schema.sh
+++ .circleci/deploy-schema.sh
@@ -3,11 +3,16 @@ set -e
 
 pwd
 REMOTE="https://github.com/zzak/b"
+GH_EMAIL="mail@zzak.io"
+GH_NAME="zzak"
 
 echo `date '+%Y-%m-%d %H:%M:%S'` > data
 
 git clone $REMOTE
 cd b
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
 cp -a ../data .
 
 git add -A
